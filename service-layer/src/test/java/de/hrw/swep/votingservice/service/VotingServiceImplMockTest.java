10ab87f2f2c95b3ca523afbfa71a90ad1e21d7ab
tk-142142 <tk-142142@AP086169.hrw.hs-weingarten.de>
2017-01-26 12:31:04 +0100

    fdsdsfsdfs

--- service-layer/src/test/java/de/hrw/swep/votingservice/service/VotingServiceImplMockTest.java
+++ service-layer/src/test/java/de/hrw/swep/votingservice/service/VotingServiceImplMockTest.java
@@ -43,7 +43,7 @@ public class VotingServiceImplMockTest {
 		when(dbReadMock.getVotesForQuestion(2)).thenReturn(votes);
 		when(dbReadMock.getStatusOfQuestion(2)).thenReturn(false);
 
-		
+///		
 
 //shsahahahas
 		votingService = new VotingServiceImpl(dbReadMock, dbWriteMock);
